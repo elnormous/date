b120d964d3b2348168ab99d93fac173d02af7ac0
Elviss Strazdins <elviss@elviss.lv>
2022-05-20 00:43:48 +0300

    Implement year parser

--- include/date.hpp
+++ include/date.hpp
@@ -1,14 +1,56 @@
 #ifndef DATE_HPP
 #define DATE_HPP
 
-#include <chrono>
+#include <stdexcept>
 #include <string>
 
 namespace date
 {
-    std::chrono::system_clock parse(std::string_view s)
+    class ParseError final: public std::runtime_error
     {
-        return std::chrono::system_clock{};
+    public:
+        using std::runtime_error::runtime_error;
+    };
+
+    struct Date final
+    {
+        std::size_t year;
+        std::size_t month;
+        std::size_t day;
+    };
+
+    template <class I>
+    std::pair<I, std::size_t> parseYear(const I begin, const I end)
+    {
+        std::size_t result = 0;
+        I iterator = begin;
+        result += (iterator != end && *iterator >= '0' && *iterator <= '9') ?
+            static_cast<std::size_t>((*iterator - '0') * 1000) :
+            throw ParseError{"Invalid year"};
+
+        result += (++iterator != end && *iterator >= '0' && *iterator <= '9') ?
+            static_cast<std::size_t>((*iterator - '0') * 100) :
+            throw ParseError{"Invalid year"};
+
+        result += (++iterator != end && *iterator >= '0' && *iterator <= '9') ?
+            static_cast<std::size_t>((*iterator - '0') * 10) :
+            throw ParseError{"Invalid year"};
+
+        result += (++iterator != end && *iterator >= '0' && *iterator <= '9') ?
+            static_cast<std::size_t>(*iterator - '0') :
+            throw ParseError{"Invalid year"};
+
+        return {++iterator, result};
+    }
+
+    Date parse(std::string_view s)
+    {
+        Date result;
+        result.year = 2022;
+        result.month = 01;
+        result.day = 01;
+
+        return result;
     }
 }
 
--- test/tests.cpp
+++ test/tests.cpp
@@ -2,8 +2,18 @@
 #include "catch2/catch.hpp"
 #include "date.hpp"
 
-TEST_CASE("Test", "[parsing]")
+TEST_CASE("January 1, 2022", "[parsing]")
 {
     const auto result = date::parse("2022-01-01");
+    REQUIRE(result.year == 2022);
+    REQUIRE(result.month == 1);
+    REQUIRE(result.day == 1);
+}
 
+TEST_CASE("Parse year", "[parsing]")
+{
+    const std::string s = "2023";
+    const auto [i, result] = date::parseYear(s.begin(), s.end());
+    REQUIRE(i == s.end());
+    REQUIRE(result == 2023);
 }
