0fe31ebaf28b4167ccc77aecd59410fb645d0364
Elviss Strazdins <elviss@elviss.lv>
2022-05-19 23:39:19 +0300

    Add test project

---
+++ include/date.hpp
@@ -0,0 +1,8 @@
+#ifndef DATE_HPP
+#define DATE_HPP
+
+namespace date
+{
+}
+
+#endif // DATE_HPP
---
+++ test/tests.cpp
@@ -0,0 +1,8 @@
+#include <cstddef>
+#include <vector>
+#include "catch2/catch.hpp"
+#include "date.hpp"
+
+TEST_CASE("Test", "[parsing]")
+{
+}
