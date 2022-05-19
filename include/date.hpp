17fbb57c8ee407d7456ec35f6c2c1718e7a3f2f3
Elviss Strazdins <elviss@elviss.lv>
2022-05-20 00:25:16 +0300

    Add parse function

--- include/date.hpp
+++ include/date.hpp
@@ -1,8 +1,15 @@
 #ifndef DATE_HPP
 #define DATE_HPP
 
+#include <chrono>
+#include <string>
+
 namespace date
 {
+    std::chrono::system_clock parse(std::string_view s)
+    {
+        return std::chrono::system_clock{};
+    }
 }
 
 #endif // DATE_HPP
--- test/tests.cpp
+++ test/tests.cpp
@@ -1,8 +1,9 @@
 #include <cstddef>
-#include <vector>
 #include "catch2/catch.hpp"
 #include "date.hpp"
 
 TEST_CASE("Test", "[parsing]")
 {
+    const auto result = date::parse("2022-01-01");
+
 }
